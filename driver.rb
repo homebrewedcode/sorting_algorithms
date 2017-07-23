123d6325a99f2a304b6e6ea52c811e972c6e22e4
Dale Pollitt <dale.negativezero@gmail.com>
2017-07-23 07:48:33 -0700

    create empirical test function and helper functions

--- driver.rb
+++ driver.rb
@@ -21,52 +21,67 @@ end
 def build_sorting_array n, range
 
   arr_merge = generate_array n, range
+  arr_bucket = copy_array arr_merge
   arr_counting = copy_array arr_merge
   arr_radix = copy_array arr_merge
 
-  array_hash = { merge: arr_merge, counting: arr_counting, radix: arr_radix }
+  array_hash = { merge: arr_merge, bucket: arr_bucket, counting: arr_counting, radix: arr_radix }
 end
 
 
 
 # test for correctness
-def empirical_tests
-  array_hash = build_sorting_array 1_000, 1_000
-  pass, fail = 0, 0
+def empirical_tests tests
+  # builds a hash of arrays to sort.
+  # keys are :merge, :counting, :radix
 
-  array_test = lambda do |sort, arr|
-    1000.times do
-      sort.sort(arr)
-      loop = arr.length - 1
+  pass = { merge: 0, bucket: 0, counting: 0, radix: 0 }
+  fail = { merge: 0, bucket: 0, counting: 0, radix: 0 }
 
-      loop.times do |val|
-        if arr[val] > arr[val + 1]
-          fail += 1
-          break
-        end
+  # run sorting test, based on passed sort algoorithm and array and record results
+  array_test = lambda do |sort, arr, key|
+    sort.sort(arr)
+    loop = arr.length - 1
+
+    loop.times do |val|
+      if arr[val] > arr[val + 1]
+        fail += 1
+        break
       end
-      pass += 1 if fail.zero?
     end
+
+    pass[key] += 1 if fail[key].zero?
   end
 
-  print_screen = lambda do |sort_name|
+  # lambda for printing results
+  print_screen = lambda do |sort_name, key|
     puts "===============#{sort_name} Tests======================"
-    puts "Pass: #{pass}\n"
-    puts "Fail: #{fail}\n"
+    puts "Pass: #{pass[key]}\n"
+    puts "Fail: #{fail[key]}\n"
   end
 
-  array_test.call(MergeSort.new, array_hash[:merge])
-  print_screen.call("Merge Sort")
-  pass, fail = 0, 0
-  array_test.call(BucketSort.new, array_hash[:counting])
-  print_screen.call("Counting Sort")
-  pass, fail = 0, 0
-  array_test.call(RadixSort.new, array_hash[:radix])
-  print_screen.call("Radix Sort")
+  # run the tests based on the parameter tests
+  tests.times do
+    array_hash = build_sorting_array 1_000, 1_000
+    array_test.call(MergeSort.new, array_hash[:merge], :merge)
+    array_test.call(BucketSort.new, array_hash[:bucket], :bucket)
+    array_test.call(CountingSort.new, array_hash[:counting], :counting)
+    array_test.call(RadixSort.new, array_hash[:radix], :radix)
+  end
+
+  print_screen.call("Merge Sort", :merge)
+  print_screen.call("Bucket Sort", :bucket)
+  print_screen.call("Counting Sort", :counting)
+  print_screen.call("Radix Sort", :radix)
+
 
 end
 
-empirical_tests
+empirical_tests 1000
+
+
+
+
 
 # n = arr_merge.length
 # MergeSort.sort(arr_merge, 0, n - 1)
